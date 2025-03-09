cc4e66fc64765b49f747d60c990a1e22e689ca9b
Mohammed Dastagir Shaikh <dastagirshaikh9519@gmail.com>
2025-03-10 00:57:40 +0530

    emailjs auth

--- lib/email.ts
+++ lib/email.ts
@@ -1,8 +1,8 @@
 import { init } from '@emailjs/browser';
 
-export const EMAILJS_SERVICE_ID = "service_8lyo5be";
-export const EMAILJS_TEMPLATE_ID = "template_ikchjyy"; // Replace with your actual template ID
-export const EMAILJS_PUBLIC_KEY = "XpK474YJyvr3Ej17g";
+export const EMAILJS_SERVICE_ID = "service_zo9w3ab";
+export const EMAILJS_TEMPLATE_ID = "template_huxfdgz"; // Replace with your actual template ID
+export const EMAILJS_PUBLIC_KEY = "xA-5b6J4CZxhkPb4B"
 
 export const initEmailJS = () => {
   init(EMAILJS_PUBLIC_KEY);
